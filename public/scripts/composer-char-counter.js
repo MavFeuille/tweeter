06c6621e668e0e7530ad52328e956666c71f9886
MavFeuille <mavis.feuille@gmail.com>
2021-09-07 23:19:44 -0400

    Create HTML markup and CSS styling for the tweet container (not new tweets)

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,11 @@ $(document).ready(function() {
   console.log("Ready to count!");
   $(".textarea").on("input", function(event) {
     const tweetLength = event.target.value.length;
+    const textLimit = 140
     console.log("tweetLength: ", tweetLength);
 
-    $(".counter").text(140 - tweetLength);
-    if (tweetLength > 140) {
+    $(".counter").text(textLimit - tweetLength);
+    if (tweetLength > textLimit) {
       $(".counter").css("color", "red");
     } else {
       $(".counter").css("color", "black");
